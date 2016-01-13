d1468aaa587a82923731c66db7e483057a590d7c
Braelan <bbraelan@gmail.com>
2016-01-13 13:54:24 -0800

    first refactor

--- Rbayes.R
+++ Rbayes.R
@@ -6,39 +6,7 @@ library('wordcloud')
 library('tm')
 library('e1071')
 
-# create a corpus of plaintextdocuments labeled that they are disease articles
-cloud <- Corpus(DirSource("/media/braelan/302F-6E98/training/positivetext/"))
-cloud <- tm_map(cloud, stripWhitespace)
-cloud <- tm_map(cloud, tolower)
-cloud <- tm_map(cloud, PlainTextDocument)
-cloud <- tm_map(cloud, removePunctuation)
-cloud <- tm_map(cloud, removeWords, stopwords("english"))
-cloud <- tm_map(cloud, stemDocument)
-cloud_dtm <- DocumentTermMatrix(cloud)
-meta(cloud, "disease") <- "yes"
-
-# create a corpus of non-disease articles
-normCloud <- Corpus(DirSource("/media/braelan/302F-6E98/training/negativetext/"))
-normCloud <- tm_map(normCloud, stripWhitespace)
-normCloud <- tm_map(normCloud, tolower)
-normCloud <- tm_map(normCloud, PlainTextDocument)
-normCloud <- tm_map(normCloud, removePunctuation)
-normCloud <- tm_map(normCloud, removeWords, stopwords("english"))
-normCloud <- tm_map(normCloud, stemDocument)
-normCloud_dtm <- DocumentTermMatrix(normCloud)
-meta(normCloud, "disease") <- "no"
-
-cleanCorpus <- function(Directory)
-normCloud <- Corpus(DirSource("/media/braelan/302F-6E98/training/negativetext/"))
-normCloud <- tm_map(normCloud, stripWhitespace)
-normCloud <- tm_map(normCloud, tolower)
-normCloud <- tm_map(normCloud, PlainTextDocument)
-normCloud <- tm_map(normCloud, removePunctuation)
-normCloud <- tm_map(normCloud, removeWords, stopwords("english"))
-normCloud <- tm_map(normCloud, stemDocument)
-normCloud_dtm <- DocumentTermMatrix(normCloud)
-meta(normCloud, "disease") <- "no"
-
+# read all the files from a directory and process them
 cleanCorpus <- function(directory) {
   normCloud <- Corpus(DirSource(directory))
   normCloud <- tm_map(normCloud, stripWhitespace)
@@ -49,20 +17,33 @@ cleanCorpus <- function(directory) {
   return( tm_map(normCloud, stemDocument))
 }
 
-raw_test <- cleanCorpus("/media/braelan/302F-6E98/training/testtext/")
+#make a document term matrices showing word frequency for negatives, positives, and a test set
+raw_negatives <- cleanCorpus("/media/braelan/302F-6E98/training/negativetext/")
+meta(raw_negatives, "disease") <- "no"
+negative_dtm <-DocumentTermMatrix(raw_negatives)
 
-newCloud <- c(cloud, normCloud)
+raw_positives <- cleanCorpus("/media/braelan/302F-6E98/training/positivetext/")
+meta(raw_positives, "disease") <- "yes"
+positive_dtm <-DocumentTermMatrix(raw_negatives)
+
+raw_negative_test <- cleanCorpus("/media/braelan/302F-6E98/training/negativetesttext/")
+#negative_test_dtm <-DocumentTermMatrix(raw_negative_test)
+raw_positive_test <- cleanCorpus("/media/braelan/302F-6E98/training/positivetesttext/")
+#positive_test_dtm <-DocumentTermMatrix(raw_positive_test)
+
+
+newCloud <- c(raw_positives, raw_negatives)
 newCloud_dtm <- DocumentTermMatrix(newCloud)
-test_train <-DocumentTermMatrix(raw_test)
+
+
 
 allFreq5 <- findFreqTerms(newCloud_dtm, 5) 
 all_train <- DocumentTermMatrix(newCloud, control= list( dictionary = allFreq5))
-test_train <- DocumentTermMatrix(raw_test, control= list(dictionary = allFreq5))
+positive_test <- DocumentTermMatrix(raw_positive_test, control= list(dictionary = allFreq5))
+negative_test <- DocumentTermMatrix(raw_negative_test, control= list(dictionary = allFreq5))
 
 
-
-
-#turn counts of words into binaries
+#turn counts of words into binaries that can be used for bayes
 count_to_binary <- function(x)  {
   y <- ifelse( x>0 , 1 , 0)
   y <- factor(y, levels=c(0,1), labels=c("No", "Yes"))
@@ -70,12 +51,14 @@ count_to_binary <- function(x)  {
 }
 
 all_train <-apply(all_train, 2, count_to_binary)
-test_train <- apply(test_train, 2, count_to_binary)
-
+positive_test <- apply(positive_test, 2, count_to_binary)
+negative_test <- apply(negative_test, 2, count_to_binary)
 
 
 disease_classifier <- naiveBayes(all_train, factor(meta(newCloud)$disease))
 
-disease_test_pred <- predict(disease_classifier, newdata=test_train)
+disease_test_pred <- predict(disease_classifier, newdata=negative_test)
+normal_test_pred <- predict(disease_classifier, newdata=positive_test)
+
 
 
--- cleanfiles.rb
+++ cleanfiles.rb
@@ -7,28 +7,81 @@ def writeFile(textfile, directory, item)
     textfile.write(nokowiki.css('#mw-content-text')[0].text)
 end
 
-def writeTextFiles(directory)
-    dir = 0
-    Dir.foreach(directory) do |item|
-      next if item == '.'  || item == ".."
+# def writeTextFiles(directory)
+#     dir = 0
+#     makeDirectories(directory)
+#
+#     Dir.foreach(directory) do |item|
+#       next if item == '.'  || item == ".."
+#
+#       n = rand * 10
+#       if n < 3
+#         outputText = File.open(directory + 'test' + 'text/' + 'clean'+ item + '.txt', 'w')
+#       else
+#         outputText = File.open(directory + 'text/' + 'clean' + item + '.txt', 'w')
+#       end
+#
+#         writeFile(outputText, directory, item)
+#
+#         dir = dir + 1
+#         if dir > 500
+#           return true
+#         end
+#
+#
+#     end
+# end
 
-      n = rand * 10
-      if n < 3
-        outputText = File.open(directory + 'test' + 'text/' + 'clean'+ item + '.txt', 'w')
-      else
-        outputText = File.open(directory + 'text/' + 'clean' + item + '.txt', 'w')
-      end
+def makeDirectories(directory)
+  # stem = directory.split('/').last
+  Dir.mkdir directory + 'testtext'
+  Dir.mkdir directory + 'text'
+end
 
-        writeFile(outputText, directory, item)
-
-        dir = dir + 1
-        if dir > 500
-          return true
-        end
-
-
-    end
+def hashDirectory(directory)
+  count = 0
+  hash = {}
+  Dir.foreach(directory) do |item|
+    next if item == '.' || item == '..'
+    hash[count] = item
+    count = count + 1
+  end
+   hash
 end
 
 
-writeTextFiles('/media/braelan/302F-6E98/training/positive')
+
+def randomArray(directoryLength, arrayLength)
+    array = []
+  0.upto(directoryLength) {|num| array << num}
+  array.shuffle!
+  array.slice(0, arrayLength)
+end
+
+
+def splitData(directory, item)
+    n = rand * 10
+    if n < 3
+      outputText = File.open(directory + 'test' + 'text/' + 'clean'+ item + '.txt', 'w')
+    else
+      outputText = File.open(directory + 'text/' + 'clean' + item + '.txt', 'w')
+    end
+      writeFile(outputText, directory, item)
+end
+
+def splitDirectory(directory, array, hash)
+  array.each do |num|
+    splitData(directory, hash[num])
+  end
+end
+
+def cleanAndPartition(directory, numberOfEndFiles)
+  makeDirectories(directory)
+  hash = hashDirectory(directory)
+  array = randomArray(hash.length, numberOfEndFiles)
+  splitDirectory(directory, array, hash)
+end
+
+
+cleanAndPartition('/media/braelan/302F-6E98/training/positive', 500)
+cleanAndPartition('/media/braelan/302F-6E98/training/negative',500)
